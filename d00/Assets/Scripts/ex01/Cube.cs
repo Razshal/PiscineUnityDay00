118ee22cc9c6049898304668e551daeb627b7e0d
Razshal <fontenille.mathieu@gmail.com>
2019-05-20 11:27:21 +0200

    ex01 - wip

---
+++ d00/Assets/Scripts/ex01/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    private float speed;
+    private float lifeTime = 3;
+    public string key = "a";
+    public GameObject downLine;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = Random.Range(0.1f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0)
+            Destroy(gameObject);
+        gameObject.transform.Translate(0, -speed, 0);
+        if (Input.GetKeyDown(key))
+        {
+            Debug.Log("Precision: " + (gameObject.transform.position.y - downLine.transform.position.y));
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ d00/Assets/Scripts/ex01/CubeSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+    public GameObject keyPrefab;
+    public float timer = 0;
+    public float spawnTimer = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnTimer)
+        {
+            Instantiate(keyPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            timer = 0;
+        }
+    }
+}
