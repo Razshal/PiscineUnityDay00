681ba26c3bd4c186084064728078f66b1762c5df
Razshal <fontenille.mathieu@gmail.com>
2019-05-20 11:58:42 +0200

    ex01 - done

--- d00/Assets/Scripts/ex01/CubeSpawner.cs
+++ d00/Assets/Scripts/ex01/CubeSpawner.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour {
-    public GameObject keyPrefab;
+    public GameObject[] cubes;
+    public GameObject[] lines;
     public float timer = 0;
-    public float spawnTimer = 3;
+    public float spawnTimer = 1;
+    public int cubeNumber = 0;
+
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+
         if (timer >= spawnTimer)
         {
-            Instantiate(keyPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            cubeNumber = Random.Range(0, cubes.Length);
+            Instantiate(cubes[cubeNumber], 
+                        lines[cubeNumber].transform.position, 
+                        lines[cubeNumber].transform.rotation);
             timer = 0;
         }
     }
